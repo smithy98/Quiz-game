7c7cccd542e085437faa60d00c5c38c5d831e82e
Smithy98 <smithy_98@outlook.com>
2024-06-16 10:47:33 +0100

    Init code

---
+++ main.py
@@ -0,0 +1,10 @@
+import inquirer
+
+print("Welcome to the history quiz!")
+
+
+question = [
+    inquirer.List("playing", message="Do you want to play?", choices=["Yes", "No"])
+]
+
+play = inquirer.prompt(question)
