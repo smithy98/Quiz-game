943b4787be3d12ce8316e7ac3e2aad14fb5ab7c7
Smithy98 <smithy_98@outlook.com>
2024-06-17 16:49:54 +0100

    Added questions

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ import inquirer
 print("Welcome to the history quiz!")
 
 
-question = [
+start = [
     inquirer.List("playing", message="Do you want to play?", choices=["Yes", "No"])
 ]
 
-play = inquirer.prompt(question)
-# test 1234
+play = inquirer.prompt(start)
+
+if play["playing"] != "Yes":
+    quit()
+
+questions = [
+    inquirer.List(
+        "Q1",
+        message="Who invaded Poland in 1939, starting WW2?",
+        choices=["Adolf Hitler", "Napoloen Bonapart", "Queen Elizabeth"],
+    ),
+    inquirer.List(
+        "Q2",
+        message="What year did the battle of Waterloo happen?",
+        choices=["1793", "1805", "1815", "1836"],
+    ),
+]
+
+question_time = inquirer.prompt(questions)
+
+print(questions)
